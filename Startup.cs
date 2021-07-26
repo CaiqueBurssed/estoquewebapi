365fd70380cabd4ed3346356391c31f96e22526d
CaiqueBurssed <caique.burssed@gmail.com>
2021-07-26 18:57:43 -0300

    edit: add automapper

--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 
 namespace EstoqueWebAPI
 {
@@ -31,7 +32,8 @@ namespace EstoqueWebAPI
 
             services.AddControllers();
 
-            //services.AddScoped<IEstoqueRepo, MockEstoqueRepo>();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
             services.AddScoped<IEstoqueRepo, SqlEstoqueRepo>();
         }
 
