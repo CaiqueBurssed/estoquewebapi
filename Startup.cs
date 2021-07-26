b53948bd01bb998565f94a2951c1186b92190887
CaiqueBurssed <caique.burssed@gmail.com>
2021-07-26 17:59:22 -0300

    add conection string to startup.cs and appsetings.json

--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +27,8 @@ namespace EstoqueWebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<EstoqueContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("EstoqueConection")));
+
             services.AddControllers();
 
             services.AddScoped<IEstoqueRepo, MockEstoqueRepo>();
