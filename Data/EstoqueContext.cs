259a42778f51518150eccd66891e6fdcbc5be7f3
CaiqueBurssed <caique.burssed@gmail.com>
2021-07-26 17:48:27 -0300

    add: data estoquecontext

---
+++ Data/EstoqueContext.cs
@@ -0,0 +1,20 @@
+﻿using EstoqueWebAPI.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EstoqueWebAPI.Data
+{
+    public class EstoqueContext : DbContext
+    {
+        public EstoqueContext(DbContextOptions<EstoqueContext> opt)  : base(opt)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
