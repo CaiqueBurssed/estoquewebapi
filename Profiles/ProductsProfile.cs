45002268141cba7ad759d2ac9c48ec8db306e268
CaiqueBurssed <caique.burssed@gmail.com>
2021-07-26 19:07:35 -0300

    add: profiles productsprofile

---
+++ Profiles/ProductsProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using EstoqueWebAPI.Dtos;
+using EstoqueWebAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EstoqueWebAPI.Profiles
+{
+    public class ProductsProfile : Profile
+    {
+        public ProductsProfile()
+        {
+            CreateMap<Product, ProductReadDto>();
+        }
+    }
+}
